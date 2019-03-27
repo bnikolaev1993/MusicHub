65629a1367e4cc4e1cf5dca61fb4c7c26342f770
Bogdan Nikolaev <bogdan.nikolaev@electrolux.com>
2019-03-27 10:17:13 +0300

    init commit

---
+++ MusicHub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicHub
+//
+//  Created by Bogdan Nikolaev on 27/03/2019.
+//  Copyright © 2019 Bogdan Nikolaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
